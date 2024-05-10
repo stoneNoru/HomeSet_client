9dc9f967df22ca3f880b8cb8c2222adc8bb3a9ec
nwy98 <nwy98@naver.com>
2024-05-10 15:11:52 +0900

    [Env] 카카오맵 api

--- src/components/KakaoMap.jsx
+++ src/components/KakaoMap.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Map } from "react-kakao-maps-sdk";
 
 const KakaoMap = () => {
-  return <div>KakaoMap</div>;
+  return (
+    <div>
+      <Map center={{ lat: 33.5563, lng: 126.79581 }} style={{ width: "800px", height: "600px" }} level={3}></Map>
+    </div>
+  );
 };
 
 export default KakaoMap;

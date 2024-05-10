d0676ca71fe7a552df83a04902aa17a039e9ed56
nwy98 <nwy98@naver.com>
2024-05-10 14:25:03 +0900

    [Feat] 페이지 라우팅

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import "./styles/App.css";
+import Home from "./pages/Home";
+import Onboarding from "./pages/Onboarding";
+import MyPage from "./pages/MyPage";
+import Error from "./pages/Error";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Routes>
+        <Route path="/" element={<Onboarding />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </div>
   );
 }

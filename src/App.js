37f0290f430d4ab4e82b87f53f7c97cb397e8f23
nwy98 <nwy98@naver.com>
2024-05-13 15:05:58 +0900

    [Feat] 중첩 라우팅 경로 설정

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import Home from "./pages/Home";
 import Onboarding from "./pages/Onboarding";
 import MyPage from "./pages/MyPage";
 import Error from "./pages/Error";
+import Transactions from "./pages/Transactions";
+import Subscription from "./pages/Subscription";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Onboarding />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<Home />}>
+          <Route path="transactions" element={<Transactions />} />
+          <Route path="subscription" element={<Subscription />} />
+        </Route>
         <Route path="/mypage" element={<MyPage />} />
         <Route path="*" element={<Error />} />
       </Routes>
